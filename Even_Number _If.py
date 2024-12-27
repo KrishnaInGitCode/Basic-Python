5313f5e78a4063af20544bf62755449bad881476
Krishna <149478303+KrishnaInGitCode@users.noreply.github.com>
2024-12-28 08:49:13 +1030

    Add files via upload

---
+++ Even_Number _If.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 28 08:18:07 2024
+
+@author: krish
+"""
+number = int(input("Please enter a number: "))
+if number % 2 == 0:
+    print("It is an even number")
+else:
+    print("It is an odd number")
