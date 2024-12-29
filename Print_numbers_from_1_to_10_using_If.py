ad9e5c44bba62c49a2c83cfbc66201f4fabbcc5e
Krishna <149478303+KrishnaInGitCode@users.noreply.github.com>
2024-12-29 20:39:08 +1030

    Add files via upload

---
+++ Print_numbers_from_1_to_10_using_If.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 29 18:23:26 2024
+
+@author: krish
+"""
+#Using for loop 
+#Print numbers from 1 to 10 using 
+# Initialize the starting number
+
+n= int(input("Enter your Number:")) 
+
+for i in range(1, n+1 , 1 ):
+    print (i)
+    i+=1
---
+++ Print_numbers_from_1_to_10_using_While.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 29 14:52:46 2024
+
+@author: krish
+"""
+#Using while loop 
+#Print numbers from 1 to 10 using 
+# Initialize the starting number
+i = 1
+n= int(input("Enter your Number:"))
+# Use a while loop to print numbers from 1 to 10
+while i <= n:
+    print(i)
+    i += 1  # Increment i by 1
+
+
