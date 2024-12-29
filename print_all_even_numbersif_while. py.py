e1128922efbe7569d012741fdff0cf1ff1bd5c0b
Krishna <149478303+KrishnaInGitCode@users.noreply.github.com>
2024-12-29 21:05:17 +1030

    Add files via upload

---
+++ print_all_even_numbersif_while. py.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 29 20:48:44 2024
+
+@author: krish
+"""
+number=int(input("Enter your number:"))
+i=1
+while i <= number :
+    if  i % 2 == 0:
+        print(i)
+    i+=1
